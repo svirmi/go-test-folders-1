3690467076c876b3bf82fcc681ae91dc53acec92
M S <mixa.ru.ru@gmail.com>
2021-04-29 08:56:03 +0300

    send json response

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Bob", "New Yourk", "192256"},
+		{"Alice", "Denver", "725609"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
