3c6f4fa781e271efd9f1affb5649c494ac6233af
M S <mixa.ru.ru@gmail.com>
2021-04-29 09:04:20 +0300

    json/xml response

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
 }
 
 func main() {
@@ -26,9 +27,13 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{"Alice", "Denver", "725609"},
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(customers)
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
