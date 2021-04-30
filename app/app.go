76e46c51cf55d36d3457e9fdd612c5449d31cdf5
M S <mixa.ru.ru@gmail.com>
2021-04-30 09:27:48 +0300

    different methods added

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -10,8 +11,20 @@ func Start() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request sent")
 }
