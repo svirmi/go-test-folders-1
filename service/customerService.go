eef090c07d760b8c7a23f121fe654c3f2cec70e1
M S <mixa.ru.ru@gmail.com>
2021-05-11 08:24:24 +0300

    start using DTO

--- service/customerService.go
+++ service/customerService.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"folders-one/domain"
+	"folders-one/dto"
 	"folders-one/errs"
 )
 
 type CustomerService interface {
 	GetAllCustomers(status string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -31,6 +32,21 @@ func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerSer
 	return DefaultCustomerService{repository}
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	response := dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.Status,
+	}
+
+	return &response, nil
 }
